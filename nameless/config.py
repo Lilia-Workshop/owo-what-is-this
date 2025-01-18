a021411641b15aea1099e8683802326609e3acd2
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-01-19 01:21:44 +0700

    config with typing

--- nameless/config.py
+++ nameless/config.py
@@ -1,12 +1,43 @@
+from datetime import datetime
 from pathlib import Path
+from typing import NotRequired, TypedDict
 
 from tomllib import loads
 
 __all__ = ["nameless_config"]
+
+
+class NamelessInfo(TypedDict):
+    version: str
+    description: str
+    support_server: str
+    start_time: datetime
+    is_shutting_down: bool
+
+
+class NamelessBlacklist(TypedDict):
+    users: list[int]
+    guilds: list[int]
+
+
+class WavelinkNode(TypedDict):
+    uri: str
+    password: str
+    region: NotRequired[str]
+    is_default: NotRequired[bool]
+    autostart: NotRequired[bool]
+
+
+class NamelessConfig(TypedDict):
+    nameless: NamelessInfo
+    blacklist: NamelessBlacklist
+    wavelinks: list[WavelinkNode]
+
 
 _cfg_path: Path = Path(__file__).parent.parent.absolute() / "nameless.toml"
 
 with open(_cfg_path, encoding="utf-8") as f:
     _content: str = f.read()
 
-nameless_config = loads(_content)
+raw_config = loads(_content)
+nameless_config: NamelessConfig = NamelessConfig(**raw_config)  # pyright: ignore[reportAny]
